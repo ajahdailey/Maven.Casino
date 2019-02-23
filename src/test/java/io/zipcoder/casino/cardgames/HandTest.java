47f249cebf30ff0380d93e1e3cebfe9ef99a444f
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-22 19:08:55 -0500

    Swapna changes with one test

--- src/test/java/io/zipcoder/casino/cardgames/HandTest.java
+++ src/test/java/io/zipcoder/casino/cardgames/HandTest.java
@@ -1,6 +1,10 @@
 package io.zipcoder.casino.cardgames;
 
+import io.zipcoder.casino.player.Player;
 import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -8,6 +12,21 @@ public class HandTest {
 
     @Test
     public void discardCard() {
+        //Given
+        Player player;
+        Hand cardsInHand;
+        Card card1 = new Card(1);
+        List<Card> list = new ArrayList<Card>();
+        list.add(card1);
+
+        //When
+        Hand hand=new Hand();
+        //String actual = hand.discardCard();
+        hand.discardCard(card1);
+
+
+        //
+
     }
 
     @Test
@@ -18,3 +37,4 @@ public class HandTest {
     public void addCardToHand() {
     }
 }
+
