047a465b0a593461bae297445111bf9b8c4f1536
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-22 19:04:02 -0500

    Swapna changes with one test

---
+++ src/test/java/io/zipcoder/casino/cardgames/HandTest.java
@@ -0,0 +1,20 @@
+package io.zipcoder.casino.cardgames;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HandTest {
+
+    @Test
+    public void discardCard() {
+    }
+
+    @Test
+    public void clearAllCardsInHand() {
+    }
+
+    @Test
+    public void addCardToHand() {
+    }
+}
