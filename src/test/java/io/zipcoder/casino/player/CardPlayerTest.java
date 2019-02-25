f7bcbd4ff1e45435de361f5aab48e1c5fc4d38f1
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-MacBook-Pro.local>
2019-02-25 16:43:13 -0500

    Swapna Hand and CardPlayer test cases

--- src/test/java/io/zipcoder/casino/player/CardPlayerTest.java
+++ src/test/java/io/zipcoder/casino/player/CardPlayerTest.java
@@ -6,6 +6,10 @@ import io.zipcoder.casino.cardgames.SignType;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import static org.junit.Assert.*;
 
 public class CardPlayerTest {
@@ -99,6 +103,102 @@ public class CardPlayerTest {
 
         //Then
         Assert.assertEquals((Integer)1, actual);
+
+    }
+
+    @Test
+    public void getName() {
+        //Given
+        Player player = new Player(0,"Swapna");
+        CardPlayer cardPlayer = new CardPlayer(player);
+        String expected = "Swapna";
+
+
+        //When
+        String actual = cardPlayer.getName();
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getRandomCardFromHand() {
+        //Given
+        Player player = new Player(0,"Swapna");
+        CardPlayer cardPlayer = new CardPlayer(player);
+        Card card = new Card(1);
+        Card card1 = new Card(2);
+        Card card2 = new Card(4);
+        cardPlayer.addCardToHand(card);
+        cardPlayer.addCardToHand(card1);
+
+
+
+        //When
+        System.out.println(cardPlayer.getRandomCardFromHand());
+
+    }
+
+    @Test
+    public void removeCardFromHand() {
+        //Given
+        Player player = new Player(0,"Swapna");
+        CardPlayer cardPlayer = new CardPlayer(player);
+        Card card = new Card(1);
+        Card card1 = new Card(2);
+        cardPlayer.addCardToHand(card);
+        cardPlayer.addCardToHand(card1);
+        Integer expected = 1;
+
+        //When
+       cardPlayer.removeCardFromHand(card);
+       Integer actual = cardPlayer.getNumberOfCardsInHand();
+
+       //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void hasCardTest() {
+        //Given
+        Player player = new Player(0,"Swapna");
+        CardPlayer cardPlayer = new CardPlayer(player);
+        Card card = new Card(1);
+        Card card1 = new Card(2);
+        cardPlayer.addCardToHand(card);
+        cardPlayer.addCardToHand(card1);
+        Boolean expected = true;
+
+        //When
+        Boolean actual = cardPlayer.hasCard(card);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void getHandCardsTest() {
+        //Given
+        Player player = new Player(0,"Swapna");
+        CardPlayer cardPlayer = new CardPlayer(player);
+        Card card = new Card(1, SignType.Diamond);
+        Card card1 = new Card(2, SignType.Clover);
+        Card card2 = new Card(12, SignType.Heart);
+        cardPlayer.addCardToHand(card);
+        cardPlayer.addCardToHand(card1);
+        cardPlayer.addCardToHand(card2);
+
+        //When
+        Card[] expectedArray =  new Card[]{card, card1, card2};
+        String expectedString = Arrays.toString(expectedArray);
+        String actualString = cardPlayer.getHandCards().toString();
+
+        //Then
+        Assert.assertEquals(expectedString, actualString);
+
+
 
     }
 }
